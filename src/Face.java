cfb83297ac3ea29c82ba2e8b7999328f9130255c
napaolella <42978497+napaolella@users.noreply.github.com>
2018-09-12 14:06:54 -0400

    Add files via upload

---
+++ src/Face.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @author Nathan Paolella
+ * Smiley Face
+ * Period 7
+ *
+ */
+
+
+public class Face {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\\\\\\\\\\\");
+		System.out.println("| * * |");
+		System.out.println("|  *  |");
+		System.out.println("| \\_/ |");
+		System.out.println("\\_____/");
+		
+
+	}
+
+}
